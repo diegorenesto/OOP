6c30ffa38ac1245895999b7586c03c93af346480
taccione <diego.renesto@iisviolamarchesini.edu.it>
2024-12-20 20:10:09 +0100

    Aggiornato submodule

---
+++ Diego/src/Main.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+
+
+        System.out.println("\n\nJava Project");
+        System.out.println(LocalDate.now());
+
+
+        // SOURCE CODE
+        
+
+    }
+}
